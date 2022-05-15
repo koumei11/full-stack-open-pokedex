132dd8bb9240f00704c17fd1dfb7785cf87d6885
KOMEI KINJO <miketyson40166@gmail.com>
2022-05-16 00:12:52 +0900

    Test Protection

--- cypress/integration/api.cypress.js
+++ cypress/integration/api.cypress.js
@@ -1,17 +1,17 @@
-describe('Pokedex', function () {
-  it('front page can be opened', function () {
-    cy.on('uncaught:exception', error => {
-      console.error(error)
-    })
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
+// describe('Pokedex', function () {
+//   it('front page can be opened', function () {
+//     cy.on('uncaught:exception', error => {
+//       console.error(error)
+//     })
+//     cy.visit('http://localhost:5000')
+//     cy.contains('ivysaur')
+//     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+//   })
 
-  it('pokemon page can be navigated to', () => {
-    cy.visit('http://localhost:5000')
-    cy.get('.list-item:nth-child(2)').click()
-    cy.contains('chlorophyll')
-  })
+//   it('pokemon page can be navigated to', () => {
+//     cy.visit('http://localhost:5000')
+//     cy.get('.list-item:nth-child(2)').click()
+//     cy.contains('chlorophyll')
+//   })
 
-})
+// })
